306f7fe0facbe4c1b5243f81f2d9d7ad701c0460
FRIK42 <43886060+FRIK42@users.noreply.github.com>
2026-01-06 12:25:52 +0300

    Объединение сосудов для синглплей

---
+++ kubejs/server_scripts/phial_merge.js
@@ -0,0 +1,16 @@
+ServerEvents.recipes(event => {
+	event.shapeless("hexcasting:battery", [Item.of("hexcasting:battery"), Item.of("hexcasting:battery")]).modifyResult((grid, result) => {
+		let phials = grid.findAll(Item.of("hexcasting:battery").ignoreNBT())
+		let media = 0
+		let nbt
+		phials.forEach(phial => {
+			media += phial.nbt["hexcasting:media"]
+			if (phial.nbt != null) nbt = phial.nbt
+		});
+		if (nbt == undefined) return itemstack
+		media = Math.min(Math.max(media,0),2000000000)
+		nbt["hexcasting:media"] = Math.trunc(media*Math.sqrt(media/2000000000))
+		nbt["hexcasting:start_media"] = Math.trunc(media)
+		return result.withNBT(nbt)
+	});
+});
