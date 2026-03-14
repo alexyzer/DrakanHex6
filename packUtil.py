63d15bfbe05cb2d3ba4afdecb64fc8357346b5b0
alexyzer <132060385+alexyzer@users.noreply.github.com>
2026-03-14 20:44:05 +0700

    Revert "Слияние серверной и клентской сборок"
    This reverts commit 6a7e37034a07a94df24f64877673f652f544e3c8.

--- packUtil.py
+++ packUtil.py
@@ -20,89 +20,99 @@ def makePwToml(download: dict, filename: str, name: str, option: dict = None, si
     if update: toml["update"] = update
     return toml
 
+def scanProjectForVersion(projectId:str, sha1:str = None):
+    response = requests.get(f"https://api.modrinth.com/v2/project/{projectId}/version", {
+        "loaders": json.dumps(["fabric"]),
+        "game_versions": json.dumps(["1.20.1"]),
+        "include_changelog": "false"
+    })
+    if response.status_code == 200:
+        for version in response.json():
+            for versionFile in version["files"]:
+                if not versionFile["primary"]: continue
 
-def optimizePack(modsFolder:str):
-    dirToReturn = os.getcwd()
-    os.chdir(modsFolder)
-    modsMetadata = {}
+                if sha1 is None or versionFile["hashes"]["sha1"] == sha1:
+                    return version, versionFile
+
+    return None, None
+
+
+def askModrinth(name:str, sha1:str = None):
+    for project in requests.get("https://api.modrinth.com/v2/search", params={
+        "query": name,
+        "facets": json.dumps([
+            ["project_type:mod"], ["categories:fabric"], ["versions:1.20.1"]
+        ]),
+        "limit": 1,
+    }).json()["hits"]:
+        version, versionFile = scanProjectForVersion(project["project_id"], sha1)
+        if versionFile: return project, version, versionFile
+    return None, None, None
+
+
+if __name__ == "__main__":
+    print(os.getcwd())
+    os.chdir("pack/mods")
     for path in os.scandir():
         if not path.is_file(): continue
 
         #Добавление обновлений к .pw.toml
         if path.name.endswith(".pw.toml"):
-            pwToml = tomllib.load(open(path, "br+"))
-            if "update" not in pwToml or True:
-                fileHash = pwToml["download"]["hash"]
-                modsMetadata[fileHash] = {"filename": path.name, "local": path.name}
-                if "option" in pwToml: modsMetadata[fileHash]["option"] = pwToml["option"]
+            file = open(path, "br+")
+            pwToml = tomllib.load(file)
+            if "update" in pwToml: continue
+            print(f"Обработка {pwToml["name"]}", end='')
+            option = pwToml.get("option")
 
+            project, version, versionFile = askModrinth(name=pwToml["name"], sha1=pwToml["download"]["hash"])
+            if project is None:
+                print(" неуспешно")
+                continue
+            print()
+
+            pwToml["name"] = project["title"]
+            pwToml["update"] = {
+                "modrinth":{
+                    "mod-id":project["project_id"],
+                    "version":version["id"]
+                }
+            }
+            file.seek(0)
+            tomli_w.dump(pwToml, file)
+            file.truncate()
 
         #Обработка .jar-ников
         elif path.name.endswith(".jar"):
-            #print(f"Хеширование {path.name}", end=' ')
-            fileHash = hashlib.sha1(open(path, "rb").read()).hexdigest()
-            modsMetadata[fileHash] = {"filename":path.name, "local":path.name}
-            #print(fileHash)
+            fabricModJson = json.load(zipfile.ZipFile(path, "r").open("fabric.mod.json", "r"))
+            print(f"Обработка {fabricModJson["name"]} ({fabricModJson["id"]})",end='')
+            project, version, versionFile = askModrinth(
+                name=fabricModJson["id"],
+                sha1=hashlib.sha1(open(path, "rb").read()).hexdigest()
+            )
+            if versionFile:
+                tomli_w.dump(
+                    makePwToml(
+                        download={
+                            "hash-format": "sha1",
+                            "hash": versionFile["hashes"]["sha1"],
+                            "url": versionFile["url"]
+                        },
+                        filename=versionFile["filename"],
+                        name=project["title"],
+                        update={
+                            "modrinth":{
+                                "modrinth": {
+                                    "mod-id": project["project_id"],
+                                    "version": version["id"]
+                                }
+                            }
+                        }
+                    ),
+                    open(project["slug"]+".pw.toml", "wb")
+                )
+                os.remove(path.name)
+                print()
+            else: print(" неуспешно")
 
         #Чистка отключённых модов
         elif path.name.endswith(".disabled"): os.remove(path.name)
-
-
-    modrinthVersions = requests.post("https://api.modrinth.com/v2/version_files", json={"hashes":list(modsMetadata), "algorithm": "sha1"}).json()
-    for fileHash, version in modrinthVersions.items():
-        metadata = modsMetadata[fileHash]
-        metadata["project_id"] = version["project_id"]
-        metadata["version_id"] = version["id"]
-        for file in version["files"]: #Поиск соответствующего файла
-            if file["hashes"]["sha1"] == fileHash:
-                metadata["filename"] = file["filename"]
-                metadata["url"] = file["url"]
-                break
-
-    modrinthProjects = requests.get("https://api.modrinth.com/v2/projects",{
-        "ids": json.dumps([
-            metadata["project_id"] for metadata in modsMetadata.values() if "project_id" in metadata
-        ])
-    }).json()
-    projectsMetadata = {
-        project["id"]:{
-            "slug":project["slug"],
-            "title":project["title"],
-            "side":["both","client","server"][len(project["client_side"])==8|(len(project["server_side"])==8)<<1]
-        } for project in modrinthProjects
-    }
-    for metadata in modsMetadata.values():
-        if "project_id" in metadata:
-            id = metadata["project_id"]
-            metadata["slug"] = projectsMetadata[id]["slug"]
-            metadata["name"] = projectsMetadata[id]["title"]
-            metadata["side"] = projectsMetadata[id]["side"]
-
-    for fileHash, metadata in modsMetadata.items():
-        if "url" not in metadata:
-            print(f"{metadata["filename"]} нет на Modrinth")
-            continue
-        pwToml = makePwToml(
-            download={
-                "hash-format": "sha1",
-                "hash": fileHash,
-                "url": metadata["url"]
-            },
-            filename=metadata["filename"],
-            name=metadata["name"],
-            side=metadata["side"],
-            update={
-                "modrinth": {
-                    "mod-id": metadata["project_id"],
-                    "version": metadata["version_id"]
-                }
-            }
-        )
-        if "option" in metadata: pwToml["option"] = metadata["option"]
-        if "local" in metadata: os.remove(metadata["local"])
-        tomli_w.dump(pwToml, open(metadata["slug"]+".pw.toml", "wb"))
-
-    os.chdir(dirToReturn)
-
-if __name__ == "__main__":
-    optimizePack("pack/mods")
